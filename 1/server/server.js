92dcc114f8c9897be43da17088fde425654e4bf4
Kashif2004K <blackwatch4321@gmail.com>
2026-07-13 22:47:36 +0500

    first commit

---
+++ 1/server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
