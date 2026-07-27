481d39aad100c039c0ee93e7ce53f04963fce400
Kashif2004K <blackwatch4321@gmail.com>
2026-07-27 19:44:12 +0500

    changes

--- 1/server/server.js
+++ 1/server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -9,8 +10,21 @@ console.log(process.env.MONGO_URI);
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please proide all the fields" });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, message: newProduct });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    res.status(500).json({ success: true, message: "Server Error" });
+  }
 });
 
 app.listen(port, () => {
